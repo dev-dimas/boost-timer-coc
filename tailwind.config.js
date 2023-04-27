78016643e514ab95def7ab9c7d50805a1b82a3a1
dev-dimas <75769207+dev-dimas@users.noreply.github.com>
2023-04-27 11:06:35 +0700

    Update CSS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,21 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
+    screens: {
+      xs: '240px',
+      sm: '576px',
+      md: '768px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        primary: '#121B28',
+        secondary: '#7C8584',
       },
     },
   },
