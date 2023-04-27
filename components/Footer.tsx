6b57feeadc65479f55e4484c48a7eef2f9b37688
dev-dimas <75769207+dev-dimas@users.noreply.github.com>
2023-04-27 11:07:39 +0700

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className="mt-auto flex justify-center bg-secondary pb-[50px] align-middle text-[3.5vw] sm:text-base">
+      <p className="py-3 text-white">Made with ❤ by Dimas Maulana</p>
+    </footer>
+  );
+};
+
+export default Footer;
