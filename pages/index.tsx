75577f3e631daf29bfbfb73927432fa29d4956a0
dev-dimas <75769207+dev-dimas@users.noreply.github.com>
2023-04-27 11:11:06 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,88 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Quicksand } from 'next/font/google';
+import Head from 'next/head';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+import Container from '@/components/Container';
+import Footer from '@/components/Footer';
+import FormBuilder from '@/components/FormBuilder';
+import FormLaboratory from '@/components/FormLaboratory';
+import Menu from '@/components/Menu';
+import Navbar from '@/components/Navbar';
+import { data } from '@/contents/data';
+import builderHutImage from '@/public/images/builder-hut.png';
+import laboratoryImage from '@/public/images/laboratory.png';
+import { IData } from '@/types/index';
+import { getLanguagePreferences } from '@/utils/preferences';
+
+const quicksand = Quicksand({ subsets: ['latin'] });
 
 export default function Home() {
+  const [isLaboratory, setIsLaboratory] = useState<boolean>(true);
+  const [isEnglish, setIsEnglish] = useState<boolean | null>(null);
+  const [contentLanguage, setContentLanguage] = useState<IData | null>(null);
+
+  useEffect(() => {
+    const getCurrentLanguage = async () => {
+      setIsEnglish(await getLanguagePreferences());
+    };
+    getCurrentLanguage();
+  }, []);
+
+  useEffect(() => {
+    if (isEnglish === true) {
+      setContentLanguage(data.en);
+    } else if (isEnglish === false) {
+      setContentLanguage(data.id);
+    }
+  }, [isEnglish]);
+
+  const switchLanguage = (): void => {
+    setIsEnglish(!isEnglish);
+    localStorage.setItem('isEnglish', String(!isEnglish));
+  };
+
+  const switchToLaboratory = (): void => {
+    setIsLaboratory(true);
+  };
+
+  const switchToBuilder = (): void => {
+    setIsLaboratory(false);
+  };
+
+  if (isEnglish === null || contentLanguage === null) {
+    return <></>;
+  }
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <>
+      <Head>
+        <title>Boost Timer Clash of Clans</title>
+        <meta name="description" content="Tools to count boost timer in Clash of Clans" />
+        <meta name="theme-color" content="#121B28" />
+        <link rel="icon" href="/icons/favicon.ico" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
+      <Navbar isEnglish={isEnglish} switchLanguage={switchLanguage} className={quicksand.className} />
+      <main className={`${quicksand.className}`}>
+        <Container>
+          <div className="my-3 flex flex-col items-center justify-center rounded-md bg-white sm:text-base">
+            <h1 className="text-[4vw] font-bold sm:text-2xl">
+              {isLaboratory ? contentLanguage.headerLaboratoryBooster : contentLanguage.headerBuilderBooster}
+            </h1>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
+              src={isLaboratory ? laboratoryImage : builderHutImage}
+              alt={isLaboratory ? 'Laboratory Image' : 'Builder Hut Image'}
+              height={200}
               priority
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            {isLaboratory ? <FormLaboratory content={contentLanguage} /> : <FormBuilder content={contentLanguage} />}
+          </div>
+        </Container>
+        <Menu content={contentLanguage} isLaboratory={isLaboratory} switchToBuilder={switchToBuilder} switchToLaboratory={switchToLaboratory} />
+      </main>
+      <Footer />
+    </>
+  );
 }
