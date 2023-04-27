401c23d3d7bf1c11176bafc6bc269c7e950a0c08
dev-dimas <75769207+dev-dimas@users.noreply.github.com>
2023-04-27 11:08:54 +0700

    Create IData types

---
+++ types/index.ts
@@ -0,0 +1,17 @@
+export interface IData {
+  headerLaboratoryBooster: string;
+  headerBuilderBooster: string;
+  boostDurationLeft: string;
+  buildTime: string;
+  researchTime: string;
+  laboratory: string;
+  builder: string;
+  day: string;
+  hours: string;
+  minutes: string;
+  calculate: string;
+  resultBoostDurationPt1: string;
+  resultBoostDurationPt2: string;
+  resultBoostDurationLeftPt1: string;
+  resultBoostDurationLeftPt2: string;
+}
